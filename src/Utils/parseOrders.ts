35d71c4d7e422003697ecb0b89b04455594c4d86
FelipeESchmidt <schmidtfe2@gmail.com>
2022-07-03 22:21:01 -0300

    feat: bettering orders parse and creating orderItems parse

--- src/Utils/parseOrders.ts
+++ src/Utils/parseOrders.ts
@@ -1,11 +1,28 @@
-import { normalizeOrders, OrderProps } from "../Controllers/ordersController";
+import {
+  normalizeItems,
+  normalizeOrders,
+  OrderItemsProps,
+  OrderProps,
+} from "../Controllers/ordersController";
 
-export const parseOrders = (orders: Array<any> | any): Array<OrderProps> => {
+export const parseOrders = (orders: any): Array<OrderProps> => {
   if (!orders) {
     return [];
   }
-  if (Array.isArray(orders)) {
-    return normalizeOrders(orders);
+  const ordersAsArray = Object.values(orders);
+  if (Array.isArray(ordersAsArray)) {
+    return normalizeOrders(ordersAsArray);
   }
-  return normalizeOrders([orders]);
+  return normalizeOrders([ordersAsArray]);
+};
+
+export const parseOrderItems = (items: any): Array<OrderItemsProps> => {
+  if (!items) {
+    return [];
+  }
+  const itemsAsArray = Object.values(items);
+  if (Array.isArray(itemsAsArray)) {
+    return normalizeItems(itemsAsArray);
+  }
+  return normalizeItems([itemsAsArray]);
 };
