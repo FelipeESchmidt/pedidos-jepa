950d1f93748df2ffe356598cba34c2d0248150ec
FelipeESchmidt <schmidtfe2@gmail.com>
2022-07-08 21:44:45 -0300

    feat: bettering some app styles

--- src/Components/Order/index.styles.ts
+++ src/Components/Order/index.styles.ts
@@ -7,12 +7,13 @@ export const StyledWrapper = styled.div`
   flex-direction: column;
   padding: 20px;
   border: 1px solid ${({ theme }) => theme.colors.basic};
+  box-shadow: 2px 2px 0 0 ${({ theme }) => theme.colors.basic};
   border-radius: 10px;
   box-sizing: border-box;
 `;
 
 export const StyledNewItemButton = styled(Button)`
-  padding: 15px 0;
+  margin-top: 20px;
   font-size: ${({ theme }) => theme.fontSizes.big};
   color: ${({ theme }) => theme.colors.secondary};
   border: 2px solid ${({ theme }) => theme.colors.secondary};
--- src/Styles/GlobalStyles.ts
+++ src/Styles/GlobalStyles.ts
@@ -14,7 +14,7 @@ const GlobalStyle = createGlobalStyle`
 `;
 
 export const WholePage = styled.div`
-  width: 100vw;
+  width: 100%;
   height: 100vh;
   background-color: ${({ theme }) => theme.colors.background};
 `;
